9f7ce6656c0356272a0b93d6e387b9d69ad07db3
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-07-22 04:37:13 +0200

    Implemented basic initialization, shutdown and update flow

--- Tubes.cpp
+++ Tubes.cpp
@@ -1,5 +1,51 @@
 #include "Tubes.h"
+#include <utility/PlatformDefinitions.h>
+#include "TubesUtility.h"
 
-Tubes::Tubes() {
+#if PLATFORM == PLATFORM_WINDOWS
+	#include <utility/RetardedWindowsIncludes.h>
+	#pragma comment( lib, "Ws2_32.lib" ) // TODODB: See if this can be done through cmake instead
+#elif PLATFORM == PLATFORM_LINUX
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <fcntl.h>
+#endif
 
+bool Tubes::Initialize() {
+#if PLATFORM == PLATFORM_WINDOWS
+	WSADATA wsaData;
+	m_Initialized = WSAStartup( MAKEWORD( 2, 2 ), &wsaData ) == NO_ERROR; // Initialize WSA version 2.2
+	if ( !m_Initialized ) {
+			LogErrorMessage( "Failed to initialize Tubes since WSAStartup failed" );	
+	}
+#else 
+	m_IsInitialized = true;
+#endif
+
+	if ( m_Initialized ) {
+		LogInfoMessage( "Tubes was successfully initialized" );
+	}
+
+	return m_Initialized;
+}
+
+void Tubes::Shutdown() {
+	if ( m_Initialized ) {
+		#if PLATFORM == PLATFORM_WINDOWS
+			WSACleanup();
+		#endif
+		LogInfoMessage( "Tubes has been shut down" );
+	} else {
+		LogWarningMessage( "Attempted to shut down uninitialized isntance of Tubes" );
+	}
+
+	m_Initialized = false;
+}
+
+void Tubes::Update() {
+	if ( m_Initialized ) {
+
+	} else {
+		LogWarningMessage( "Attempted to update uninitialized instance of Tubes" );
+	}
 }
--- Tubes.h
+++ Tubes.h
@@ -3,8 +3,10 @@
 
 class Tubes {
 public:
-	TUBES_API Tubes();
+	TUBES_API bool Initialize();
+	TUBES_API void Shutdown();
+	TUBES_API void Update();
 
 private:
-	
+	bool m_Initialized = false;
 };
