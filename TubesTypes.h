f5da0e8dc88c30dbaa14a86339c5896e91241f17
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-07-31 16:43:25 +0200

    Added listener struct to TubesTypes. Also added socklen_t typdef for Windows and a connection state enum which will be used in a later commit

--- TubesTypes.h
+++ TubesTypes.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdint.h>
+#include <thread>
+#include <atomic>
 #include <utility/PlatformDefinitions.h>
+#include "TubesErrors.h"
 
 #if PLATFORM != PLATFORM_WINDOWS // winsock already defines this on windows
 #define INVALID_SOCKET	~0
@@ -10,3 +13,29 @@
 typedef uint32_t	Address;
 typedef uint16_t	Port;
 typedef int64_t		Socket;
+
+#if PLATFORM == PLATFORM_WINDOWS
+typedef int socklen_t;
+#endif
+
+enum ConnectionState {
+	NEW_OUT,
+	NEW_IN,
+};
+
+struct Listener {
+	Listener() {
+		Thread = nullptr;
+		ListeningSocket = INVALID_SOCKET;
+		ShouldTerminate = pNew( std::atomic_bool, false );
+	}
+
+	~Listener() {
+		pDelete( Thread );
+		pDelete( ShouldTerminate );
+	}
+
+	Socket				ListeningSocket;
+	std::thread*		Thread;
+	std::atomic_bool*	ShouldTerminate;
+};
