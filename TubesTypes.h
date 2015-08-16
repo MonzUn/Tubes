67d4216fc642a4f61752b5cdc81a60069f8f6ea5
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-08-16 21:04:17 +0200

    Changed the type of ConnectionID to int32_t and added a define for invalid ID

--- TubesTypes.h
+++ TubesTypes.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <utility/CallbackRegister.h>
 
-typedef uint32_t	ConnectionID;
+#define INVALID_CONNECTION_ID -1
+typedef int32_t	ConnectionID;
 
 struct	ConnectionCallbackTag {};
 typedef Handle<ConnectionCallbackTag, int, -1>	ConnectionCallbackHandle;
