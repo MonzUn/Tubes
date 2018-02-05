13df97d1896bbd50cfb9e88ba5d8ac4f2477e94e
MonzUn <daniel-bengtsson@live.com>
2018-02-05 23:51:34 +0100

    Updated MUtility

--- include/MUtility/MUtilityFile.h
+++ include/MUtility/MUtilityFile.h
@@ -1,6 +1,14 @@
 #pragma once
+#include <string>
 
 namespace MUtilityFile
 {
-	bool CreateFolder(const char* directoryPath);
+	bool CreateDir(const char* directoryPath); // Cannot be named "CreateDirectory" due to conflict with windows.h
+
+	bool DirectoryExists(const char* folderPath);
+	bool FileExists(const char* filePath);
+
+	std::string GetDirectoryPathFromFilePath(const std::string& filePath);
+	std::string	GetFileContentAsString(const std::string& filePath);
+	uint64_t GetFileContentSize(const std::string& filePath);
 }
